6816ff8b678fb19ffca41cf1908705246891bd18
ChristopherLBruce <37425633+ChristopherLBruce@users.noreply.github.com>
2018-12-18 17:51:45 -0600

    more formatting

--- PyLEDStrip/color.py
+++ PyLEDStrip/color.py
@@ -1,18 +1,9 @@
-"""
-Wrapper for tuple class used to replicate neopixel module functionality
+""" Color class for storing and manipulating RGB color values """
 
-**Author:**
-
-   Chris Bruce, chris.bruce@dsvolition.com, 12/3/2018
-"""
-# from functools import wraps
-from types import MethodType
 from collections import namedtuple
 
 
-
 Color = namedtuple('Color', ('r, g, b'))
-
 
 
 def __add__(self, color):
@@ -54,59 +45,93 @@ def __mul__(self, color):
 	return Color( r, g, b )
 
 
-
-def _clamp( self, value ):
+def clamp(self, value):
+	"""Ensures value are between 0 and 255"""
 	return sorted( ( 0, abs( int( value ) ), 255 ) )[ 1 ]
 
 
-def add( self, color ):
+def add(self, color):
+	"""Adds color to self
+
+	Arguments:
+		color {Color} -- Color value to blend with self. If a float or int is
+			given instead, that is converted to r, g, b values.
+
+	Returns:
+		self -- The instance of this object
+	"""
+
 	if isinstance( color, Color ):
 		r, g, b	= color
 	elif isinstance( color, float ) or isinstance( color, int ):
 		r, g, b	= [ int( 255.0 * color ) ] * 3
 
-	self.r = self._clamp(self.r + r)
-	self.g = self._clamp(self.g + g)
-	self.b = self._clamp(self.b + b)
+	self.r = self.clamp(self.r + r)
+	self.g = self.clamp(self.g + g)
+	self.b = self.clamp(self.b + b)
 
 	return self
 
 
 def subtract(self, color):
+	"""Subtracts color from self"
+
+	Arguments:
+		color {Color} -- Color value to blend with self. If a float or int is
+			given instead, that is converted to r, g, b values.
+
+	Returns:
+		self -- The instance of this object
+	"""
+
 	if isinstance(color, Color):
 		r, g, b = color
 	elif isinstance(color, float) or isinstance(color, int):
 		r, g, b = [ int( 255.0 * color ) ] * 3
 
-	self.r = self._clamp(self.r - r)
-	self.g = self._clamp(self.g - g)
-	self.b = self._clamp(self.b - b)
+	self.r = self.clamp(self.r - r)
+	self.g = self.clamp(self.g - g)
+	self.b = self.clamp(self.b - b)
 
 	return self
 
 
 def multiply(self, color):
+	"""Multiplies color with self
+
+	Arguments:
+		color {Color} -- Color value to blend with self. If a float or int is
+			given instead, that is converted to r, g, b values.
+
+	Returns:
+		self -- The instance of this object
+	"""
+
 	if isinstance(color, Color):
 		r, g, b = color
 	elif isinstance(color, float) or isinstance(color, int):
 		r, g, b = [int(255.0 * color)] * 3
 
-	self.r = self._clamp(self.r * r)
-	self.g = self._clamp(self.g * g)
-	self.b = self._clamp(self.b * b)
+	self.r = self.clamp(self.r * r)
+	self.g = self.clamp(self.g * g)
+	self.b = self.clamp(self.b * b)
 
 	return self
 
 
 def blend(self, color, bias = 0.5):
+	"""Blends color with self
+
+	Arguments:
+		color {Color} -- Color value to blend with self. If a float or int is
+			given instead, that is converted to r, g, b values.
+
+	Keyword Arguments:
+		bias {float} -- Bias value for blending color with self (default: {0.5})
+
+	Returns:
+		self -- The instance of this object
 	"""
-	Blends two colors using a bias value
-
-	**Author:**
-
-		Chris Bruce, chris.bruce@dsvolition.com, 12/3/2018
-	"""
-
 	if isinstance(color, Color):
 		r, g, b = color
 	elif isinstance(color, float) or isinstance(color, int):
@@ -117,9 +142,9 @@ def blend(self, color, bias = 0.5):
 	g = (g * inv) + (self.g * bias)
 	b = (b * inv) + (self.b * bias)
 
-	self.r = self._clamp(r)
-	self.g = self._clamp(g)
-	self.b = self._clamp(b)
+	self.r = self.clamp(r)
+	self.g = self.clamp(g)
+	self.b = self.clamp(b)
 
 	return self
 
@@ -128,7 +153,7 @@ def blend(self, color, bias = 0.5):
 Color.__add__ = __add__
 Color.__sub__ = __sub__
 Color.__mul__ = __mul__
-Color._clamp = _clamp
+Color.clamp = clamp
 Color.add = add
 Color.subtract = subtract
 Color.multiply = multiply
@@ -136,12 +161,12 @@ Color.blend = blend
 
 
 
-
+# Test stuff
 red = Color(255, 0, 0)
 green = Color(0, 255, 0)
 white = Color(255, 255, 255)
 black = Color(255, 255, 255)
 print(red + green)
-# red.blend( green, bias = 0.5)
-red.b = 255
-print( red.r )
+
+red.blend( green, bias = 0.5 )
+print( red )		# should return Color(r=127, g=127, b=0)
