22310218fa34800d1a1ddc11df5a2ff1b871d5ec
ChristopherLBruce <37425633+ChristopherLBruce@users.noreply.github.com>
2018-12-18 20:06:15 -0600

    refactored Color class

--- PyLEDStrip/color.py
+++ PyLEDStrip/color.py
@@ -1,172 +1,97 @@
 """ Color class for storing and manipulating RGB color values """
 
-from collections import namedtuple
+class Color():
+	"""Color class to store and manipulate RGB values """
+
+	__slots__ = 'r', 'g', 'b'
+
+	def __init__(self, r, g, b):
+			"""Create new instance of Color(r, g, b)"""
+			self.r = r
+			self.g = g
+			self.b = b
 
 
-Color = namedtuple('Color', ('r, g, b'))
+	def __repr__(self):
+		"""Returns a nicely formatted representation string"""
+		return '({}, {}, {})'.format( self.r, self.g, self.b )
 
 
-def __add__(self, color):
-	if isinstance(color, Color):
-		r, g, b = color
-	elif isinstance(color, float) or isinstance(color, int):
-		r, g, b = [int(255.0 * color)] * 3
-
-	r += self.r
-	g += self.g
-	b += self.b
-
-	return Color( r, g, b )
+	def __iter__(self):
+		"""Itterator for RGB values"""
+		for x in [ self.r, self.g, self.b ]:
+			yield x
 
 
-def __sub__(self, color):
-	if isinstance(color, Color):
-		r, g, b = color
-	elif isinstance(color, float) or isinstance(color, int):
-		r, g, b = [int(255.0 * color)] * 3
+	def __add__(self, color):
+		"""method for '+' operator"""
+		if isinstance(color, Color):
+			r, g, b = color.r, color.g, color.b
+		elif isinstance(color, float) or isinstance(color, int):
+			r, g, b = [int(255 * color)] * 3
 
-	r -= self.r
-	g -= self.g
-	b -= self.b
+		r = self._clamp( self.r + r )
+		g = self._clamp( self.g + g )
+		b = self._clamp( self.b + b )
 
-	return Color( r, g, b )
-
-
-def __mul__(self, color):
-	if isinstance(color, Color):
-		r, g, b = color
-	elif isinstance(color, float) or isinstance(color, int):
-		r, g, b = [int(255.0 * color)] * 3
-
-	r *= self.r
-	g *= self.g
-	b *= self.b
-
-	return Color( r, g, b )
+		return Color(r, g, b)
 
 
-def clamp(self, value):
-	"""Ensures value are between 0 and 255"""
-	return sorted( ( 0, abs( int( value ) ), 255 ) )[ 1 ]
+	def __sub__(self, color):
+		"""method for '-' operator"""
+		if isinstance(color, Color):
+			r, g, b = color.r, color.g, color.b
+		elif isinstance(color, float) or isinstance(color, int):
+			r, g, b = [int(255 * color)] * 3
+
+		r = self._clamp( self.r - r )
+		g = self._clamp( self.g - g )
+		b = self._clamp( self.b - b )
+
+		return Color(r, g, b)
 
 
-def add(self, color):
-	"""Adds color to self
+	def __mul__(self, color):
+		"""method for '*' operator"""
+		if isinstance(color, Color):
+			r, g, b = color.r, color.g, color.b
+		elif isinstance(color, float) or isinstance(color, int):
+			r, g, b = [int(255 * color)] * 3
 
-	Arguments:
-		color {Color} -- Color value to blend with self. If a float or int is
-			given instead, that is converted to r, g, b values.
+		r = self._clamp(int((self.r / 255) * (r / 255) * 255))
+		g = self._clamp(int((self.g / 255) * (g / 255) * 255))
+		b = self._clamp(int((self.b / 255) * (b / 255) * 255))
 
-	Returns:
-		self -- The instance of this object
-	"""
-
-	if isinstance( color, Color ):
-		r, g, b	= color
-	elif isinstance( color, float ) or isinstance( color, int ):
-		r, g, b	= [ int( 255.0 * color ) ] * 3
-
-	self.r = self.clamp(self.r + r)
-	self.g = self.clamp(self.g + g)
-	self.b = self.clamp(self.b + b)
-
-	return self
+		return Color(r, g, b)
 
 
-def subtract(self, color):
-	"""Subtracts color from self"
-
-	Arguments:
-		color {Color} -- Color value to blend with self. If a float or int is
-			given instead, that is converted to r, g, b values.
-
-	Returns:
-		self -- The instance of this object
-	"""
-
-	if isinstance(color, Color):
-		r, g, b = color
-	elif isinstance(color, float) or isinstance(color, int):
-		r, g, b = [ int( 255.0 * color ) ] * 3
-
-	self.r = self.clamp(self.r - r)
-	self.g = self.clamp(self.g - g)
-	self.b = self.clamp(self.b - b)
-
-	return self
+	def _clamp(self, value):
+		"""Ensures RGB value are between 0 and 255"""
+		return sorted( ( 0, abs( int( value ) ), 255 ) )[ 1 ]
 
 
-def multiply(self, color):
-	"""Multiplies color with self
+	def blend(self, color, bias = 0.5):
+		"""Blends color with self
 
-	Arguments:
-		color {Color} -- Color value to blend with self. If a float or int is
-			given instead, that is converted to r, g, b values.
+		Arguments:
+			color {Color} -- Color value to blend with self. If a float or int is
+				given instead, that is converted to r, g, b values.
 
-	Returns:
-		self -- The instance of this object
-	"""
+		Keyword Arguments:
+			bias {float} -- Bias value for blending color with self (default: {0.5})
 
-	if isinstance(color, Color):
-		r, g, b = color
-	elif isinstance(color, float) or isinstance(color, int):
-		r, g, b = [int(255.0 * color)] * 3
+		Returns:
+			self -- The instance of this object
+		"""
 
-	self.r = self.clamp(self.r * r)
-	self.g = self.clamp(self.g * g)
-	self.b = self.clamp(self.b * b)
+		if isinstance(color, Color):
+			r, g, b = color.r, color.g, color.b
+		elif isinstance(color, float) or isinstance(color, int):
+			r, g, b = [int(255 * color)] * 3
 
-	return self
+		inv = 1 - bias
+		r = self._clamp(int(self.r * bias) + int(r * inv))
+		g = self._clamp(int(self.g * bias) + int(g * inv))
+		b = self._clamp(int(self.b * bias) + int(b * inv))
 
-
-def blend(self, color, bias = 0.5):
-	"""Blends color with self
-
-	Arguments:
-		color {Color} -- Color value to blend with self. If a float or int is
-			given instead, that is converted to r, g, b values.
-
-	Keyword Arguments:
-		bias {float} -- Bias value for blending color with self (default: {0.5})
-
-	Returns:
-		self -- The instance of this object
-	"""
-	if isinstance(color, Color):
-		r, g, b = color
-	elif isinstance(color, float) or isinstance(color, int):
-		r, g, b = [int(255.0 * color)] * 3
-
-	inv = 1 - bias
-	r = (r * inv) + (self.r * bias)
-	g = (g * inv) + (self.g * bias)
-	b = (b * inv) + (self.b * bias)
-
-	self.r = self.clamp(r)
-	self.g = self.clamp(g)
-	self.b = self.clamp(b)
-
-	return self
-
-
-# extend Color class methods
-Color.__add__ = __add__
-Color.__sub__ = __sub__
-Color.__mul__ = __mul__
-Color.clamp = clamp
-Color.add = add
-Color.subtract = subtract
-Color.multiply = multiply
-Color.blend = blend
-
-
-
-# Test stuff
-red = Color(255, 0, 0)
-green = Color(0, 255, 0)
-white = Color(255, 255, 255)
-black = Color(255, 255, 255)
-print(red + green)
-
-red.blend( green, bias = 0.5 )
-print( red )		# should return Color(r=127, g=127, b=0)
+		return Color(r, g, b)
